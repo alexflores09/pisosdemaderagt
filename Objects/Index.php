dfe40d475c35a992f102daefa9c31e4fa54f6837
SH4DOW-BET4\estua <estuardo.samayoa502@gmail.com>
2017-05-31 23:21:08 -0600

    Cambio en el color del fondo(body) al color del fondo del prototipo, Cammbios en el index(pendientes cambios en el slider por imagenes.

--- Objects/Index.php
+++ Objects/Index.php
@@ -13,6 +13,29 @@ class Index implements Object {
     }
 
     public function draw(){
-        print "Hola Mundo";
+        ?>
+        <div class="col s12 center">
+            <h4 style="color: #B71C1C">Bienvenido a Pisos de Madera GT</h4>
+            <p>Contamos con mas de 20 años de experiencia , brindando asesoría, soluciones y presentándoles las mejores opciones a nuestros clientes, desarrollando proyectos grandes, desde edificios completos hasta una sola habitación, dándole a cada uno de ellos la mas alta y fina calidad, con esto hemos logrado ganarnos la confianza de nuestros clientes.</p>
+        </div>
+        <div class="container">
+            <div class="carousel carousel-slider">
+            <!-- Prueba -->
+                <a class="carousel-item" style="cursor: pointer;"><img src="img/logo.jpg" alt=""></a>
+                <a class="carousel-item" style="cursor: pointer;"><img src="img/logo.png" alt=""></a>
+                <a class="carousel-item" style="cursor: pointer;"><img src="img/construccion.jpg" alt=""></a>
+                <!--Despues de arreglar Imagenes
+                <a class="carousel-item" style="cursor: pointer;"><img src="img/escaleras/escaleras-1.jpg" alt=""></a>
+                <a class="carousel-item" style="cursor: pointer;"><img src="img/pisos/piso-1.jpg" alt=""></a>
+                <a class="carousel-item" style="cursor: pointer;"><img src="img/otros/otros-1.jpg" alt=""></a>-->
+            </div>
+        </div>
+        <script>
+            $(document).ready(function(){
+                $('.carousel').carousel();
+                $('.carousel.carousel-slider').carousel({fullWidth: true});
+            });
+        </script>
+        <?php
     }
 }
