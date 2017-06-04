16a357af2648a188c9a68c3d177a6614e8ceef60
SH4DOW-BET4\estua <estuardo.samayoa502@gmail.com>
2017-06-03 20:13:51 -0600

    Cambios en el index, se agrego funcion para que el slider se desplazara automaticamente

--- Objects/Index.php
+++ Objects/Index.php
@@ -14,26 +14,48 @@ class Index implements Object {
 
     public function draw(){
         ?>
-        <div class="col s12 center">
-            <h4 style="color: #B71C1C">Bienvenido a Pisos de Madera GT</h4>
-            <p>Contamos con mas de 20 años de experiencia , brindando asesoría, soluciones y presentándoles las mejores opciones a nuestros clientes, desarrollando proyectos grandes, desde edificios completos hasta una sola habitación, dándole a cada uno de ellos la mas alta y fina calidad, con esto hemos logrado ganarnos la confianza de nuestros clientes.</p>
+        <style>
+            footer {
+                margin-top: 50px;
+            }
+            .divFLoatSlider{
+                background: #EFEFEF;
+                height: 290px;
+                color: #B71C1C;
+            }
+            .slider-reponsive{
+                width: 85%;
+            }
+            .carousel{
+                height: 400px!important;
+            }
+            @media screen and (max-width: 768px){
+                .slider-reponsive{
+                    width: 80%;
+                }
+            }
+        </style>
+        <div class="col s12 center" style="margin-top: 50px;">
+            <h3 style="color: #B71C1C;"><strong>Bienvenido a Pisos de Madera GT</strong></h3>
+            <p class="flow-text" style="text-align: justify;">Contamos con mas de 20 años de experiencia , brindando asesoría, soluciones y presentándoles las mejores opciones a nuestros clientes, desarrollando proyectos grandes, desde edificios completos hasta una sola habitación, dándole a cada uno de ellos la mas alta y fina calidad, con esto hemos logrado ganarnos la confianza de nuestros clientes.</p>
         </div>
-        <div class="container">
-            <div class="carousel carousel-slider">
-            <!-- Prueba -->
-                <a class="carousel-item" style="cursor: pointer;"><img src="img/logo.jpg" alt=""></a>
-                <a class="carousel-item" style="cursor: pointer;"><img src="img/logo.png" alt=""></a>
-                <a class="carousel-item" style="cursor: pointer;"><img src="img/construccion.jpg" alt=""></a>
-                <!--Despues de arreglar Imagenes
+        <div style="display: flex;">
+            <div class="carousel carousel-slider center slider-reponsive">
                 <a class="carousel-item" style="cursor: pointer;"><img src="img/escaleras/escaleras-1.jpg" alt=""></a>
-                <a class="carousel-item" style="cursor: pointer;"><img src="img/pisos/piso-1.jpg" alt=""></a>
-                <a class="carousel-item" style="cursor: pointer;"><img src="img/otros/otros-1.jpg" alt=""></a>-->
+                <a class="carousel-item" style="cursor: pointer;"><img src="img/pisos/piso-7.jpg" alt=""></a>
+                <a class="carousel-item" style="cursor: pointer;"><img src="img/otros/otros-1.jpg" alt=""></a>
+            </div>
+            <div class="divFLoatSlider" style="margin-bottom: 50px;">
+                <h5 style="padding: 50px 5px;" class="flox-text center"><strong>Materiales <br>de<br> Calidad</strong></h5>
+                <hr style="border: 3px solid #FFFFFF;">
+                <h5 style="padding: 50px 5px;" class="flox-text center"><strong>Satisfacción <br> Garantizada</strong></h5>
             </div>
         </div>
         <script>
             $(document).ready(function(){
-                $('.carousel').carousel();
+                $('.carousel').carousel({dist:0});
                 $('.carousel.carousel-slider').carousel({fullWidth: true});
+                window.setInterval(function(){$('.carousel').carousel('next')},4000);
             });
         </script>
         <?php
